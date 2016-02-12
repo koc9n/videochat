13636bd0ab150006415130a9f575bc4e56c4c5e0
koc9n <kos.mironchik@gmail.com>
2016-02-13 00:13:09 +0200

    fixes for working in local and public domains;

--- config/env/production.js
+++ config/env/production.js
@@ -25,7 +25,7 @@ module.exports = {
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  port: 80
+  port: 80,
   /***************************************************************************
    * Set the log level in production environment to "silent"                 *
    ***************************************************************************/
@@ -34,4 +34,42 @@ module.exports = {
   //   level: "silent"
   // }
 
+  passport: {
+    twitter: {
+      name: 'Twitter',
+      protocol: 'oauth',
+      strategy: require('passport-twitter').Strategy,
+      options: {
+        consumerKey: 'unlTey7V2HCb93M2p7rGb6d6L',
+        consumerSecret: 'rEnFfTC92uzuPbyTNOOzSozdbSNW9R5QEvSamuAtpY2MK9gFPg',
+        callbackURL: 'http://coolapps.pp.ua/auth/twitter/callback'
+      }
+    },
+
+    facebook: {
+      name: 'Facebook',
+      protocol: 'oauth2',
+      strategy: require('passport-facebook').Strategy,
+      options: {
+        clientID: '1639063506338417',
+        clientSecret: '44a8b714f21bdf1896bb2840f832ab6f',
+        scope: ['email'],
+        profileFields: ['id', 'displayName', 'link', 'photos', 'email'],
+        callbackURL: 'http://coolapps.pp.ua/auth/facebook/callback'
+      }
+    },
+
+    vkontakte: {
+      name: 'Vkontakte',
+      protocol: 'oauth2',
+      strategy: require('vkontakte-passport').Strategy,
+      options: {
+        clientID: '4551676',
+        clientSecret: 'SgFGqj0oXyKWUDguQqAB',
+        scope: ['email'],
+        profileFields: ['screen_name','domain','photo_200_orig','sex','middle_name'],
+        callbackURL: 'http://coolapps.pp.ua/auth/vkontakte/callback'
+      }
+    }
+  }
 };
