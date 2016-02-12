cf46e2bf5fb804dfc590c3129be24a5ee8227c50
koc9n <kos.mironchik@gmail.com>
2016-02-13 00:06:58 +0200

    fixes for working in local and public domain;

--- config/env/development.js
+++ config/env/development.js
@@ -20,6 +20,45 @@ module.exports = {
   // models: {
   //   connection: 'someMongodbServer'
   // }
-  port: 80
+  port: 80,
+
+  passport: {
+    twitter: {
+      name: 'Twitter',
+      protocol: 'oauth',
+      strategy: require('passport-twitter').Strategy,
+      options: {
+        consumerKey: 'unlTey7V2HCb93M2p7rGb6d6L',
+        consumerSecret: 'rEnFfTC92uzuPbyTNOOzSozdbSNW9R5QEvSamuAtpY2MK9gFPg',
+        callbackURL: 'http://coolapps.pp.ua/auth/twitter/callback'
+      }
+    },
+
+    facebook: {
+      name: 'Facebook',
+      protocol: 'oauth2',
+      strategy: require('passport-facebook').Strategy,
+      options: {
+        clientID: '1639063506338417',
+        clientSecret: '44a8b714f21bdf1896bb2840f832ab6f',
+        scope: ['email'],
+        profileFields: ['id', 'displayName', 'link', 'photos', 'email'],
+        callbackURL: 'http://coolapps.pp.ua/auth/facebook/callback'
+      }
+    },
+
+    vkontakte: {
+      name: 'Vkontakte',
+      protocol: 'oauth2',
+      strategy: require('vkontakte-passport').Strategy,
+      options: {
+        clientID: '4551676',
+        clientSecret: 'SgFGqj0oXyKWUDguQqAB',
+        scope: ['email'],
+        profileFields: ['screen_name','domain','photo_200_orig','sex','middle_name'],
+        callbackURL: 'http://coolapps.pp.ua/auth/vkontakte/callback'
+      }
+    }
+  }
 
 };
