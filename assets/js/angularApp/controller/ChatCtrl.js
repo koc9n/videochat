8a3977ccc08f4b00d3333f04bbb0d5eca8f24cb2
koc9n <kos.mironchik@gmail.com>
2016-04-06 16:29:28 +0700

    fixed duplicate messages after init chat

--- assets/js/angularApp/controller/ChatCtrl.js
+++ assets/js/angularApp/controller/ChatCtrl.js
@@ -8,21 +8,24 @@
   angular.module('ChatApp').controller('ChatCtrl', ['$scope', function ($scope) {
     io.sails.autoConnect = false;
     io.sails.transports = ['websocket'];
-    var socket = io.sails.connect();
-    socket.get('/chat/init');
 
+    $scope.messageArr = [];
+    if($scope.socket == null) {
+      $scope.socket = io.sails.connect();
+      $scope.socket.get('/chat/init');
+    }
 
     /**
      * Info about socket connection
      */
-    socket.on("connect", function () {
+    $scope.socket.on("connect", function () {
       console.log('Chat socket has been connected successfully.');
     });
 
     /**
      * Action by init of chat
      */
-    socket.on("init", function (msg) {
+    $scope.socket.on("init", function (msg) {
       if (msg) {
         for (var i = 0; i < msg.length; i++) {
           $scope.pushToMessages(msg[i]);
@@ -34,16 +37,14 @@
     /**
      * Receiving messages
      */
-    socket.on("chat", function onServerSentEvent(msg) {
+    $scope.socket.on("chat", function onServerSentEvent(msg) {
       $scope.pushToMessages(msg);
       $scope.$apply();
     });
 
-    $scope.messageArr = [];
-
     $scope.sendData = function () {
       if ($scope.messageTxt.trim() != "") {
-        socket.post('/chat/init', {message: $scope.messageTxt});
+        $scope.socket.post('/chat/init', {message: $scope.messageTxt});
         $scope.messageTxt = "";
       }
     };
